17e3d48fb39dd07cdbd1213fb6596d003ba15448
imsonalbajaj <imsonalbajaj@gmail.com>
2024-11-29 00:29:35 +0530

    offer view add

--- TheCaffeteria/Screens/HomeView/View/HomeView.swift
+++ TheCaffeteria/Screens/HomeView/View/HomeView.swift
@@ -13,6 +13,11 @@ struct HomeView: View {
     var body: some View {
         NavigationStack(path: $path) {
             VStack {
+                Color.white
+                    .frame(height: TOP_INSET)
+                
+                todaysOfferView
+                
                 HStack {
                     Spacer()
                 }
@@ -33,6 +38,37 @@ struct HomeView: View {
             }
             .background(Color.white)
             .navigationTitle("Home")
+        }
+        .ignoresSafeArea(.all)
+    }
+    
+    var todaysOfferView: some View {
+        ZStack(alignment: .topLeading) {
+            TabView() {
+                NetworkImageView(imageUrl: "https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg", placeHolderImg: "")
+                    .scaledToFit()
+
+                NetworkImageView(imageUrl: "https://www.foodiesfeed.com/wp-content/uploads/2023/08/tartelettes-top-view.jpg", placeHolderImg: "")
+                    .scaledToFit()
+                
+                
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            .frame(width: 300, height: 300)
+            .background(Color.black)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            
+            
+            HStack(spacing: 0) {
+                Image(systemName: "star.circle.fill")
+                    .foregroundStyle(.yellow)
+                    .padding(2)
+                    .background(Color.red.opacity(0.5))
+                    .clipShape(Circle())
+                
+                Text("today's Offers")
+                    .foregroundStyle(.white)
+            }.padding(12)
         }
     }
 }
