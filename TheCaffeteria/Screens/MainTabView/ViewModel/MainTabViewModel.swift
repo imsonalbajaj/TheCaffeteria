9fac211586963c73870f6cb2f1ccef8a89a135e7
imsonalbajaj <imsonalbajaj@gmail.com>
2024-12-04 14:48:58 +0530

    vm added

---
+++ TheCaffeteria/Screens/MainTabView/ViewModel/MainTabViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabViewModel.swift
+//  TheCaffeteria
+//
+//  Created by Sonal on 04/12/24.
+//
+
+import Foundation
+
+import SwiftUI
+
+class MainTabViewModel : ObservableObject {
+}
