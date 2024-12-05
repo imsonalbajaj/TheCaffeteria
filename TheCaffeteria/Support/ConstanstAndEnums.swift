510f2bac7c5f90379d28f15217bf40b20fc92fb8
imsonalbajaj <imsonalbajaj@gmail.com>
2024-12-05 22:18:56 +0530

    view model changes

--- TheCaffeteria/Support/ConstanstAndEnums.swift
+++ TheCaffeteria/Support/ConstanstAndEnums.swift
@@ -9,6 +9,19 @@ import UIKit
 
 // MARK: constant's
 
+// color's
+let color_dark48 = "dark48"
+let color_dark96 = "dark96"
+let color_dark144 = "dark144"
+let color_dark151 = "dark151"
+let color_dark216 = "dark216"
+let color_dark241 = "dark241"
+let color_mb_green_selected_border = "mbGreenSelectedBorder"
+let color_mb_green_selected_backGround = "mbGreenSelectedBackground"
+let color_primary_red = "primaryRed"
+let color_food_primary_green = "foodPrimaryGreen"
+let color_rating_yellow = "ratingYellow"
+
 let TOP_INSET: CGFloat = UIApplication.shared.connectedScenes
     .compactMap { $0 as? UIWindowScene }
     .flatMap { $0.windows }
